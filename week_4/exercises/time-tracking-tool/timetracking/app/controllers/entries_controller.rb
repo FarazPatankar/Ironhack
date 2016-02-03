605478adf810160b7461b0d2480ca23c9624d803
Faraz Patankar <farazpatankar@gmail.com>
2016-02-03 14:10:01 -0500

    Implement link_to

--- week_4/exercises/time-tracking-tool/timetracking/app/controllers/entries_controller.rb
+++ week_4/exercises/time-tracking-tool/timetracking/app/controllers/entries_controller.rb
@@ -17,4 +17,21 @@ class EntriesController < ApplicationController
 		@project = Project.find(params[:project_id])
 		@entry = @project.entries.new
 	end
+
+	def create
+		@project = Project.find(params[:project_id])
+		@entry = @project.entries.new entry_params
+
+		if @entry.save
+			redirect_to action: "index", controller: "entries", project_id: @project.id
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def entry_params
+		params.require(:entry).permit(:hours, :minutes, :date)
+	end
 end
