206e680813f79395c4df9fcadc03e8787aca8ab4
Faraz Patankar <farazpatankar@gmail.com>
2016-02-10 10:00:52 -0500

    Example ajax request

--- week_5/examples/ajax_request/app.js
+++ week_5/examples/ajax_request/app.js
@@ -1,14 +1,24 @@
 $(document).on("ready", function() {
 	$(".js-show-characters").on("click", function() {
 		fetchCharacters();
+	});
+
+	$(".js-add-kylo").on("click", function() {
+		publishPete();
+	});
+
+	$(".js-submit").on("click", function(event) {
+		event.preventDefault();
+		addCharacter();
 	})
-})
+});
 
 //----------------------------------
 
 function fetchCharacters() {
 	$.ajax({
 	  url: 'https://ironhack-characters.herokuapp.com/characters',
+
 	  success: function(response) {
 	  	console.log("Everything went okay!");
 
@@ -34,5 +44,80 @@ function displayCharacters(characters) {
 		`;
 
 		$(".js-characters-list").append(html);
+	});
+}
+
+function publishPete() {
+	var newCharacter = {
+		name: "Pete",
+		occupation: "Betraying friends",
+		weapon: "Pistol"
+	}
+
+	$.ajax({
+		type: "POST",
+
+		url: "https://ironhack-characters.herokuapp.com/characters",
+
+		data: newCharacter,
+
+		success: function(response) {
+			var html = `
+				<li>
+					<h2> ${newCharacter.name} </h2>
+					<p> ${response.id} </p>
+					<p>Occupation: ${newCharacter.occupation} </p>
+					<p>Weapon: ${newCharacter.weapon} </p>
+				</li>
+			`;
+
+			$(".js-characters-list").append(html);
+
+			alert("Success");
+		},
+
+		error: function() {
+			alert("Failure")
+		}
+	});
+}
+
+function addCharacter() {
+	var charName = $(".js-name").val();
+	var charOccupation = $(".js-occupation").val();
+	var charWeapon = $(".js-weapon").val();
+
+
+	var newCharacter = {
+		name: charName,
+		occupation: charOccupation,
+		weapon: charWeapon
+	}
+
+	$.ajax({
+		type: "POST",
+
+		url: "https://ironhack-characters.herokuapp.com/characters",
+
+		data: newCharacter,
+
+		success: function(response) {
+			var html = `
+				<li>
+					<h2> ${newCharacter.name} </h2>
+					<p> ${response.id} </p>
+					<p>Occupation: ${newCharacter.occupation} </p>
+					<p>Weapon: ${newCharacter.weapon} </p>
+				</li>
+			`;
+
+			$(".js-characters-list").append(html);
+
+			alert("Success");
+		},
+
+		error: function() {
+			alert("Failure");
+		}
 	});
 }
