91ba23683fb3945f39a30ccaf2faacfbb0246f4e
Faraz Patankar <farazpatankar@gmail.com>
2016-02-11 09:43:32 -0500

    Half way through the second iteration

--- week_5/examples/av/public/av.js
+++ week_5/examples/av/public/av.js
@@ -5,3 +5,25 @@ $(".js-play").on("click", function() {
 $(".js-pause").on("click", function() {
 	$(".js-vid").trigger("pause");
 });
+
+$(".js-add-vid").on("click", function() {
+	var prompt = window.prompt("Enter a link...");
+	// jPrompt('Link goes here:', '', 'Add a video', function(prompt) {
+    	if(prompt) {
+    		var html = `
+    			<video class="js-user-vid" src="${prompt}" controls></video>
+    			<button class="js-user-play">Play</button>
+    			<button class="js-user-pause">Pause</button>
+    		`;
+
+    		$(".js-user-vids").append(html);
+    	}
+    	else {
+    		alert("What is wrong with you?!")
+    	}
+	// });
+})
+
+$(".js-user-vids").on("click", ".js-user-play", function(event) {
+
+})
